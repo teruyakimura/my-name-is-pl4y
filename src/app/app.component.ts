e03994fbcee6c48eb0f9f47ef0d122c66c40f6fd
Willian Teruya Kimura <teruya.kimura@aluno.ufabc.edu.br>
2019-10-31 17:25:03 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  audio2 = new Audio(" ../../../assets/giornos.mp3");
   title = 'dont-push-the-button';
+
+  buttonClicked: boolean = false;
+
+  onClick() {
+    console.log(`oh no you didn't`);
+    this.buttonClicked = !this.buttonClicked;
+    this.playGiorno();
+  }
+
+  pause(){
+    this.audio2.pause();
+    this.buttonClicked = !this.buttonClicked;
+  }
+
+  playGiorno() {
+    let audio = new Audio();
+    audio.src = " ../../../assets/giornos.mp3";
+    this.audio2.load();
+    this.audio2.play();
+  }
 }
